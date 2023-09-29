2086580165aecd2298b485b79c8b5230510da1a0
Aditya Dhakal <dhakaladitya96@gmail.com>
2023-09-29 23:08:45 +0545

    authentication

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -13,6 +13,46 @@ export class Authservice {
         this.account = new Account(this.client);
 
     }
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            return this.login({ email, password });
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        }
+        catch (error) {
+            throw error;
+
+        }
+
+    }
+
+    async getCurrentUser() {
+        try {
+
+            return await this.account.get();
+        }
+        catch (error) {
+            throw error;
+        }
+
+    }
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        }
+        catch (error) {
+            throw error;
+        }
+    }
+
 }
 
 const authService = new Authservice();
